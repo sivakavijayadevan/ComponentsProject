bc5f295f23fcec6e8b46a23b5856c8c2154d401e
IHORSETECH\sivakavij <sivakavij@ihorsetechnologies.com>
2017-01-17 12:31:18 +0530

    tsdoc added

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   <multi-lang-label class="lblBack" [isLable]="false" [displayValue]="userTestStatus" [valueKey]="selectedlang"></multi-lang-label>`,
 })
 export class AppComponent  {
-selectedlang = 'fr';
-userTestStatus = {en: 'Hello', fr: 'Bonjour'};
+/**
+ * App Component 
+ * @class AppComponent
+ * @classdesc AppComponent is the base template class
+ */
+
+/**
+  * selectedlang property
+  * @member {string} AppComponent#selectedlang
+  */
+public selectedlang: string = 'fr';
+
+/**
+  * userTestStatus property
+  * @member {any} AppComponent#userTestStatus
+  */
+public userTestStatus: any = {en: 'Hello', fr: 'Bonjour'};
+
 }
